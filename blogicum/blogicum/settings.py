cc76d4fa7a2630204864e84cead673ebcb67eb77
Mikhail Pautov <pautovmisha3066@yandex.ru>
2026-03-09 08:52:24 +0400

    исправлено не отображение изображений

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -11,12 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xn5)z4fdefk$^j!_&-!xukk-d@@eprv!nes2l_wp5f2=6_!l(a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost'
-]
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -117,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 handler403 = 'pages.views.csrf_failure'
 handler404 = 'pages.views.page_not_found'
@@ -13,4 +14,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
     path('auth/registration/', include('pages.urls_registration')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Раздача медиафайлов — работает и при DEBUG=True и при DEBUG=False
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
